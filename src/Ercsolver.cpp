f9f5061325dfbbb75ec5bab69bfb76138f7bf13d
Marios Georgiou <mgeorgiou.ece@gmail.com>
2022-01-23 19:31:12 -0500

    format code

--- src/Ercsolver.cpp
+++ src/Ercsolver.cpp
@@ -1,19 +1,22 @@
 #include "Ercsolver.h"
 
-
-double ErcSolver::Sx(int i){
+double ErcSolver::Sx(int i)
+{
 	return (Sigma * x)(i);
 }
 
-double ErcSolver::var(int i){
-	return Sigma(i,i);
+double ErcSolver::var(int i)
+{
+	return Sigma(i, i);
 }
 
-double ErcSolver::volty(){ //the overall volatility
+double ErcSolver::volty()
+{ //the overall volatility
 	return sqrt(x.transpose() * Sigma * x);
 }
 
-void ErcSolver::setXi(int i){ //one step of the coordinate descent algorithm
+void ErcSolver::setXi(int i)
+{ //one step of the coordinate descent algorithm
 	double alpha = var(i);
 	double beta = Sx(i) - x(i) * var(i);
 	double gamma = 0 - volty() / (float)n;
@@ -21,16 +24,17 @@ void ErcSolver::setXi(int i){ //one step of the coordinate descent algorithm
 	x(i) = (0 - beta + sqrt(delta)) / (2.0 * alpha);
 }
 
-
-VectorXd ErcSolver::solve(MatrixXd S, int iterations){ //number of iterations
+VectorXd ErcSolver::solve(MatrixXd S, int iterations)
+{ //number of iterations
 	n = S.rows();
 	Sigma = S;
-	x = VectorXd::Constant(n, 1.0/(float)n); //equal initial weight
-	for (int iter = 0; iter < iterations; iter++){
-		for (int i = 0; i < n; i++){
+	x = VectorXd::Constant(n, 1.0 / (float)n); //equal initial weight
+	for (int iter = 0; iter < iterations; iter++)
+	{
+		for (int i = 0; i < n; i++)
+		{
 			setXi(i);
 		}
 	}
 	return x;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,25 @@
 using namespace std;
 using namespace Eigen;
 
-#define MAXBUFSIZE  ((int) 1e6)
+#define MAXBUFSIZE ((int)1e6)
 
 MatrixXd readMatrix(const char *filename)
-    {   
+{
     int cols = 0, rows = 0;
     double buff[MAXBUFSIZE];
 
     // Read numbers from file into buffer.
     ifstream infile;
     infile.open(filename);
-    while (! infile.eof())
-        {   
+    while (!infile.eof())
+    {
         string line;
         getline(infile, line);
 
         int temp_cols = 0;
         stringstream stream(line);
-        while(! stream.eof())
-            stream >> buff[cols*rows+temp_cols++];
+        while (!stream.eof())
+            stream >> buff[cols * rows + temp_cols++];
 
         if (temp_cols == 0)
             continue;
@@ -35,39 +35,43 @@ MatrixXd readMatrix(const char *filename)
             cols = temp_cols;
 
         rows++;
-        }   
+    }
 
     infile.close();
 
     rows--;
 
     // Populate matrix with numbers.
-    MatrixXd result(rows,cols);
+    MatrixXd result(rows, cols);
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < cols; j++)
-            result(i,j) = buff[ cols*i+j ];
+            result(i, j) = buff[cols * i + j];
 
     return result;
-    };  
+};
 
-int main(int argc, char** argv)
-	{
-    char* filename;
-    if (argc == 2){
+int main(int argc, char **argv)
+{
+    char *filename;
+    if (argc == 2)
+    {
         filename = argv[1];
     }
-    else{
+    else
+    {
         std::cout << "Usage: ./ercsolve correlation-matrix-file\n";
         return 1;
     }
     MatrixXd Sigma = readMatrix(filename);
-	if (!Sigma.isCorrelationMatrix()){
-		std::cout << "It is not a correlation matrix.\n" << std::endl;
-		return 1;
-	}
+    if (!Sigma.isCorrelationMatrix())
+    {
+        std::cout << "It is not a correlation matrix.\n"
+                  << std::endl;
+        return 1;
+    }
     ErcSolver solver;
-    VectorXd solution = solver.solve(Sigma,50);
-	std::cout << "The risk contributions are\n" << solution/solution.sum() << std::endl;
+    VectorXd solution = solver.solve(Sigma, 50);
+    std::cout << "The risk contributions are\n"
+              << solution / solution.sum() << std::endl;
     return 0;
-	}
-
+}
