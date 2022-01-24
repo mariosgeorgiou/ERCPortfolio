35972a91afbd383c3dd7507668996eb71d17dbe3
Marios Georgiou <mgeorgiou.ece@gmail.com>
2022-01-23 21:21:12 -0500

    format correlationMatrix

--- include/CorrelationMatrix.h
+++ include/CorrelationMatrix.h
@@ -1,5 +1,6 @@
-inline bool isCorrelationMatrix(void) const {
-	return (this->rows() == this->cols() && 
+inline bool isCorrelationMatrix(void) const
+{
+	return (this->rows() == this->cols() &&
 			this->isApprox(this->transpose()) &&
 			this->lpNorm<Infinity>() <= 1);
 }
